d18c7439a72ae0d9981aee64ca622f8227ebdd05
GiaPol <policolagianni@gmail.com>
2025-12-10 13:12:21 +0100

    fix: correzione path della CompactPagination

--- frontend-web/src/components/CompactPagination.tsx
+++ frontend-web/src/components/CompactPagination.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useCallback } from 'react';
-import './CompactPagination.css';
+import '../css/CompactPagination.css';
 
 interface CompactPaginationProps {
     currentPage: number;
